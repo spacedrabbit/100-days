0ad721c2f24de9539891f349bf94a71ef25ef8a1
Louis Tur <louis.tur@gmail.com>
2016-02-21 20:37:04 -0500

    Pods, snapkit, and sliders

--- One00Days/One00Days/One00DaysViewController.swift
+++ One00Days/One00Days/One00DaysViewController.swift
@@ -7,12 +7,54 @@
 //
 
 import UIKit
+import SnapKit
 
 class One00DaysViewController: UIViewController {
 
+  let animationSlider: UISlider = {
+    let slider: UISlider = UISlider()
+    slider.value = 0.0
+    return slider
+  }()
+  
+  let changingTextView: UILabel = {
+    let label: UILabel = UILabel();
+    label.textAlignment = .Center
+    label.numberOfLines = 1
+    label.font = UIFont.systemFontOfSize(24.0)
+    label.textColor = UIColor.blueColor()
+    return label;
+  }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    self.configureConstraints()
+    self.configureViews()
+  }
+  
+  internal func configureConstraints() {
+    self.view.backgroundColor = UIColor.lightGrayColor()
+    self.view.addSubview(self.animationSlider)
+    self.view.addSubview(self.changingTextView)
+    
+    self.animationSlider.snp_makeConstraints { (make) -> Void in
+      make.left.equalTo(self.view.snp_left).offset(10.0)
+      make.right.equalTo(self.view.snp_right).offset(-10.0)
+      make.bottom.equalTo(self.view.snp_bottom).offset(-10.0)
+      make.height.equalTo(60.0)
+    }
+    
+    self.changingTextView.snp_makeConstraints { (make) -> Void in
+      make.center.equalTo(self.view.snp_center)
+      make.height.equalTo(50.0)
+      make.left.equalTo(self.view.snp_left).offset(10.0)
+      make.right.equalTo(self.view.snp_right).offset(-10.0)
+    }
+  }
+  
+  internal func configureViews() {
+    self.animationSlider.addTarget(self, action: "sliderValueDidChange:", forControlEvents: .ValueChanged)
   }
 
   override func didReceiveMemoryWarning() {
@@ -20,6 +62,11 @@ class One00DaysViewController: UIViewController {
     
   }
 
+  internal func sliderValueDidChange(sender: AnyObject?) {
+    if let slider: UISlider = sender as? UISlider {
+      print("Current Value: \(slider.value)")
+    }
+  }
 
 }
 
---
+++ One00Days/Pods/Target Support Files/Pods-One00Days/Pods-One00Days-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_One00DaysVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_One00DaysVersionString[];
+
