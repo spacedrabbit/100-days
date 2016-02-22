eba1d90ce52be8c762596c78d674ac2614a03ab2
Louis Tur <louis.tur@gmail.com>
2016-02-21 20:02:38 -0500

    bare setup

---
+++ One00Days/One00Days/One00DaysViewController.swift
@@ -0,0 +1,25 @@
+//
+//  One00DaysViewController.swift
+//  One00Days
+//
+//  Created by Louis Tur on 2/21/16.
+//  Copyright © 2016 SRLabs. All rights reserved.
+//
+
+import UIKit
+
+class One00DaysViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    
+  }
+
+
+}
+
