e2c9410113ccaf677f65cf5cf731ebc28137766d
Louis Tur <louis.tur@gmail.com>
2016-02-23 23:42:06 -0500

    day three, CABasicAnimation

---
+++ One00Days/One00Days/Day3ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  Day3ViewController.swift
+//  One00Days
+//
+//  Created by Louis Tur on 2/23/16.
+//  Copyright © 2016 SRLabs. All rights reserved.
+//
+
+import UIKit
+
+class Day3ViewController: UIViewController {
+  
+  lazy var centeredView: UIView = {
+    var view: UIView = UIView()
+    view.backgroundColor = UIColor.redColor()
+    return view
+  }()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    self.setupViewHierarchy()
+    self.configureConstraints()
+    
+    self.drawCircleIn(self.centeredView)
+  }
+  
+  internal func configureConstraints() {
+    
+    self.centeredView.snp_makeConstraints { (make) -> Void in
+      make.height.equalTo(100.0)
+      make.width.equalTo(100.0)
+      make.center.equalTo(self.view.snp_center)
+    }
+    
+  }
+  
+  internal func setupViewHierarchy() {
+    self.view.addSubview(self.centeredView)
+  }
+  
+  internal func drawCircleIn(view: UIView) {
+    let shapeLayer: CAShapeLayer = CAShapeLayer()
+    let circlePath: UIBezierPath = UIBezierPath(ovalInRect: CGRectMake(0.0, 0.0, 100.0, 100.0))
+    shapeLayer.path = circlePath.CGPath
+    shapeLayer.fillColor = UIColor.blueColor().CGColor
+    view.layer.addSublayer(shapeLayer)
+    
+    let circleAnimation: CABasicAnimation = CABasicAnimation(keyPath:"strokeEnd")
+    circleAnimation.duration = 2.0
+    circleAnimation.repeatCount = 1
+    circleAnimation.fromValue = NSNumber(float: 0.0)
+    circleAnimation.toValue = NSNumber(float: 1.0)
+    
+    shapeLayer.addAnimation(circleAnimation, forKey: "drawCircleAnimation")
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+}
