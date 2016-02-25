33af8857df92e1fa68c7a6c2d4be295ab5e10e59
Louis Tur <louis.tur@gmail.com>
2016-02-24 22:54:59 -0500

    pure CoreAnimation drawing a UIControl

--- One00Days/One00Days/Day4ViewController.swift
+++ One00Days/One00Days/Day4ViewController.swift
@@ -8,15 +8,65 @@
 
 import UIKit
 
+class TappableCircleButton: UIControl {
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    self.backgroundColor = UIColor.clearColor()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
+  
+  override func drawRect(rect: CGRect) {
+    let circlePath: UIBezierPath = UIBezierPath(roundedRect: CGRectInset(rect, 4.0, 4.0), cornerRadius: rect.height/2.0)
+    let circleShapeLayer: CAShapeLayer = CAShapeLayer()
+    circleShapeLayer.path = circlePath.CGPath
+    circleShapeLayer.strokeColor = UIColor.blueColor().CGColor
+    circleShapeLayer.fillColor = UIColor.yellowColor().CGColor
+    circleShapeLayer.lineCap = kCALineCapSquare
+    circleShapeLayer.lineWidth = 4.0
+    
+    let horizontalLinePath: UIBezierPath = UIBezierPath()
+    horizontalLinePath.moveToPoint(CGPointMake(12.0, CGRectGetMidY(rect)))
+    horizontalLinePath.addLineToPoint(CGPointMake(CGRectGetMaxX(rect) - 12.0, CGRectGetMidY(rect)))
+    horizontalLinePath.closePath()
+    let horizontalLineShapeLayer: CAShapeLayer = CAShapeLayer()
+    horizontalLineShapeLayer.path = horizontalLinePath.CGPath
+    horizontalLineShapeLayer.strokeColor = UIColor.redColor().CGColor
+    horizontalLineShapeLayer.lineCap = kCALineCapSquare
+    horizontalLineShapeLayer.lineWidth = 4.0
+    
+    let verticalLinePath: UIBezierPath = UIBezierPath()
+    verticalLinePath.moveToPoint(CGPointMake(rect.width / 2.0, 12.0))
+    verticalLinePath.addLineToPoint(CGPointMake(rect.width / 2.0, rect.height - 12.0))
+    let verticalLineShapeLayer: CAShapeLayer = CAShapeLayer()
+    verticalLineShapeLayer.path = verticalLinePath.CGPath
+    verticalLineShapeLayer.strokeColor = UIColor.redColor().CGColor
+    verticalLineShapeLayer.lineCap = kCALineCapSquare
+    verticalLineShapeLayer.lineWidth = 4.0
+
+    
+    layer.addSublayer(circleShapeLayer)
+    circleShapeLayer.addSublayer(horizontalLineShapeLayer)
+    circleShapeLayer.addSublayer(verticalLineShapeLayer)
+    
+    circlePath.stroke()
+    horizontalLinePath.stroke()
+    verticalLinePath.stroke()
+  }
+}
+
 class Day4ViewController: UIViewController {
   
   lazy var contentView: UIView = {
     var view: UIView = UIView()
+    view.backgroundColor = UIColor.purpleColor()
     return view
   }()
   
-  lazy var plusButton: UIControl = {
-    var control: UIControl = UIControl()
+  lazy var plusButton: TappableCircleButton = {
+    var control: TappableCircleButton = TappableCircleButton()
     return control
   }()
   
@@ -34,12 +84,23 @@ class Day4ViewController: UIViewController {
   }
   
   internal func configureConstraints() {
+    self.contentView.snp_makeConstraints { (make) -> Void in
+      make.height.equalTo(100.0)
+      make.leading.equalTo(self.view.snp_leading)
+      make.trailing.equalTo(self.view.snp_trailing)
+      make.centerY.equalTo(self.view.snp_centerY)
+    }
     
+    self.plusButton.snp_makeConstraints { (make) -> Void in
+      make.size.equalTo(CGSizeMake(50.0, 50.0))
+      make.leading.equalTo(self.contentView.snp_leading).offset(15.0)
+      make.top.equalTo(self.contentView.snp_top).offset(15.0)
+    }
   }
   
   internal func setupViewHierarchy() {
     self.view.addSubview(self.contentView)
-    self.view.addSubview(self.plusButton)
+    self.contentView.addSubview(self.plusButton)
   }
   
   internal func configureButton() {
