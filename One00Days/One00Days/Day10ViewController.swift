755d42456ecdc37f93d2f00b4a572fc394c15459
Louis Tur <louis.tur@gmail.com>
2016-03-02 00:16:11 -0500

    done for day 10. i made a thing that shoots rects, what a day...

--- One00Days/One00Days/Day10ViewController.swift
+++ One00Days/One00Days/Day10ViewController.swift
@@ -38,46 +38,60 @@ class CubeGenerator {
 class Animator {
   internal class func animateWithUpwardsSpring(cube: UIView) {
     let currentFrame: CGRect = cube.frame
-    let minimumDistanceToCover: CGFloat = 700.0
+    let minimumDistanceToCover: CGFloat = 500.0
     
-    let xCoordRandomize: CGFloat = CGFloat(NSNumber(unsignedInt: arc4random_uniform(99) + 1).doubleValue / 100.0)
+    let xCoordRandomize: CGFloat = CGFloat(NSNumber(unsignedInt: arc4random_uniform(99) + 1).doubleValue / 100.0) * 356.0
     let yCoordRandomize: CGFloat = CGFloat(NSNumber(unsignedInt: arc4random_uniform(99) + 1).doubleValue) + minimumDistanceToCover
     
     print("\(xCoordRandomize), \(yCoordRandomize)")
-    UIView .animateWithDuration(2.0, delay: 0.0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.4, options: .BeginFromCurrentState, animations: { () -> Void in
+    UIView.animateWithDuration(2.5, delay: 0.0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.4, options: .BeginFromCurrentState, animations: { () -> Void in
       
-      let shiftedX: CGFloat = xCoordRandomize * currentFrame.origin.x
-      let shiftedY: CGFloat = yCoordRandomize
-      let newFrame: CGRect = CGRectMake(shiftedX, shiftedY, currentFrame.size.width, currentFrame.size.height)
+      let shiftedX: CGFloat = xCoordRandomize
+      let shiftedY: CGFloat = yCoordRandomize + currentFrame.origin.y
+      let newFrame: CGRect = CGRectMake(shiftedX, 568.0 - shiftedY, currentFrame.size.width, currentFrame.size.height)
       
       cube.frame = newFrame
       
       }) { (complete: Bool) -> Void in
-        
+        print("Done animating")
     }
   }
 }
 
 
 class Day10ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        self.view.backgroundColor = UIColor.whiteColor()
-      
-      let cube: UIView = CubeGenerator.createCubeWithColor(UIColor.redColor())
-      self.view.addSubview(cube)
-      cube.frame = CGRectMake(cube.frame.origin.x, CGRectGetMaxY(self.view.frame), 100.0, 100.0)
-      Animator.animateWithUpwardsSpring(cube)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  
+  lazy var button: UIButton = {
+    let button: UIButton = UIButton(frame: CGRectMake(10.0, 30.0, 200.0, 100.0))
+    return button
+  }()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-
+    self.view.backgroundColor = UIColor.whiteColor()
+//    self.view.addSubview(self.button)
+    self.button.backgroundColor = UIColor.redColor()
+    self.button.setTitle("PRESS", forState: .Normal)
+    self.button.addTarget(self, action: "fireCube", forControlEvents: UIControlEvents.TouchUpInside)
+    
+    let timer: NSTimer = NSTimer(timeInterval: 0.25, target: self, selector: "fireCube", userInfo: nil, repeats: true)
+    NSRunLoop.mainRunLoop().addTimer(timer, forMode: NSDefaultRunLoopMode)
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+  internal func fireCube() {
+    let cube: UIView = CubeGenerator.createCubeWithColor(UIColor.redColor())
+    self.view.addSubview(cube)
+    cube.frame = CGRectMake(CGRectGetMidX(cube.frame), CGRectGetMaxY(self.view.frame) + 100.0, 100.0, 100.0)
+    Animator.animateWithUpwardsSpring(cube)
+  }
+  
   internal func configureConstraints() {
     
   }
@@ -85,5 +99,5 @@ class Day10ViewController: UIViewController {
   internal func setupViewHierarchy() {
     
   }
-
+  
 }
