128818a57e491ff2c1e6fe82483e0928b0dd2d64
Louis Tur <louis.tur@gmail.com>
2016-03-04 21:30:01 -0500

    Completes day 13: drawing hexagons from a circle. additional time was used to add some explanation to the code for future reference

--- One00Days/One00Days/Day13ViewController.swift
+++ One00Days/One00Days/Day13ViewController.swift
@@ -6,6 +6,14 @@
 //  Copyright © 2016 SRLabs. All rights reserved.
 //
 
+/* references:
+https://gist.github.com/sketchytech/74727d13a2a6176914a1
+https://gist.github.com/sketchytech/746440080fb0121b0ab5
+http://www.mathopenref.com/coordparamcircle.html
+http://sketchytech.blogspot.co.uk/2014/11/the-power-of-hexagons-starting-from.html?q=drawrect&view=flipcard
+*/
+
+
 import UIKit
 import CoreGraphics
 
@@ -25,9 +33,9 @@ class Day13ViewController: UIViewController {
     self.view.layer.addSublayer(circle)
     
     self.view.addSubview(hexView)
+  
     
     let vertices: [CGPoint] = getPointsForHexagonPath(hexView)
-    
     drawHexFromPoints(vertices, view: hexView)
     
     // Do any additional setup after loading the view.
@@ -47,7 +55,7 @@ class Day13ViewController: UIViewController {
     let circleArcPaths: UIBezierPath = UIBezierPath()
     for segment in 0...6 {
       let arc: UIBezierPath = UIBezierPath(arcCenter: center, radius: radius, startAngle:degrees2Rad(1.0 * CGFloat(segment)), endAngle: degrees2Rad(60.0 * CGFloat(segment)), clockwise: true)
-      circleArcPaths.appendPath(arc)
+      circleArcPaths.appendPath(arc) // appendPath() doesn't force a closePath(), so there aren't any unwanted lines when creating the arcs
     }
     
     let circleShapeLayer: CAShapeLayer = CAShapeLayer()
@@ -86,15 +94,47 @@ class Day13ViewController: UIViewController {
   }
   
   internal func getPointsForHexagonPath(inView: UIView) -> [CGPoint] {
-    let viewCenter: CGPoint = CGPointMake(CGRectGetMidX(view.bounds), CGRectGetMidY(view.bounds))
-    let viewRadius: CGFloat = viewCenter.x
-    let numberOfPoints: Int = 6
-    let angleSize: CGFloat =  degrees2Rad(360.0 / CGFloat(numberOfPoints))
+    let viewCenter: CGPoint = CGPointMake(CGRectGetMidX(inView.bounds), CGRectGetMidY(inView.bounds)) // effectively the origin of the circle to use
+    let viewRadius: CGFloat = viewCenter.x // the radius of the circle that fits in this square
+    let numberOfPoints: Int = 6 // 6 vertices in a hexagon
+    let angleSize: CGFloat =  degrees2Rad((360.0 / CGFloat(numberOfPoints))) // size of angle for each arc in the circle, in radians. where each arc meets is effectively a point in the hexagon
     
+    
+    /*
+    As the links above explain, the parametric formula of a circle is: 
+    
+    x = r * cos(𝛳)
+    y = r * sin(𝛳)
+    
+    where:
+    r = radius of the circle
+    𝛳 = the angle in radians (corresponds to the interior angle of the line segment of (origin.x, origin.y) and (x, y) with respect to the x axis)
+    
+    If the center of the circle is not the origin point, you just have to +/- the distance from the origin to the center of the circle, resulting in something like:
+    
+    x = w - (r * cos(𝛳))
+    y = h - (r * sin(𝛳))
+
+    In this example, it's easy to calculate this distance since the view we're drawing in defines the bounds of the circle. So the radius ends up being (width/2.0)
+    Coordinate in views are measured relative to the top left corner of a view, meaning that in order to get the correct translation of the hexagon's center, we need
+    to subtract the radius from each point to shift drawing to be done relative to the center of the view
+    
+    */
     var vertexPoints: [CGPoint] = []
     for point in 1...numberOfPoints {
-      let dx: CGFloat = viewRadius * cos(CGFloat(point) * angleSize)
-      let dy: CGFloat = viewRadius * sin(CGFloat(point) * angleSize)
+      let dx: CGFloat = viewRadius - viewRadius * cos(CGFloat(point) * angleSize )
+      let dy: CGFloat = viewRadius - viewRadius * sin(CGFloat(point) * angleSize )
+      
+      /*
+      This loop will create a hexagon with it's first vertex point resting on the x-axis. if you wanted to rotate it so that this first point begins elsewhere,
+      it is possible to just add an additional amount of radians to do so. 
+      
+      // this code will rotate the hex drawing 90 degrees
+      let dx: CGFloat = viewRadius - viewRadius * cos(CGFloat(point) * angleSize + degrees2Rad(90.0) )
+      let dy: CGFloat = viewRadius - viewRadius * sin(CGFloat(point) * angleSize + degrees2Rad(90.0) )
+
+      */
+      
       
       vertexPoints.append(CGPointMake(dx, dy))
     }
