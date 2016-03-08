d6c1a9794583e15025dc0f0bc15236a54c885d74
Louis Tur <louis.tur@gmail.com>
2016-03-07 23:22:27 -0500

    project base working

--- One00Days/One00Days/Day16ViewController.swift
+++ One00Days/One00Days/Day16ViewController.swift
@@ -10,31 +10,42 @@ import UIKit
 
 class Day16ViewController: UIViewController {
   
+  let hexHelper: HexCube = HexCube()
   lazy var hexView: UIView = {
     let view: UIView = UIView()
     view.backgroundColor = UIColor.yellowColor()
     return view
   }()
   
-  
-  
   override func viewDidLoad() {
     super.viewDidLoad()
+    self.view.backgroundColor = UIColor.whiteColor()
     
-    
+    setupViewHierarchy()
+    configureConstraints()
+    drawHex()
   }
   
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
-    
+  }
+  
+  internal func drawHex() {
+    hexHelper.vertexPointsForHexCube(withOrigin: CGPointMake(self.hexView.bounds.midX, self.hexView.bounds.midY), radius:(self.hexView.frame.width / 2.0))
+    hexHelper.drawDefaultHexCube(inView: self.hexView)
   }
   
   internal func configureConstraints() {
+    self.hexView.snp_makeConstraints { (make) -> Void in
+      make.size.equalTo(CGSizeMake(200.0, 200.0))
+      make.center.equalTo(self.view.snp_center)
+    }
     
+    self.view.layoutIfNeeded()
   }
   
   internal func setupViewHierarchy() {
-    
+    self.view.addSubview(hexView)
   }
   
 }
