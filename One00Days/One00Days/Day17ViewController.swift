7fd6e929113e136d8e3e49fe01972244944619b3
Louis Tur <louis.tur@gmail.com>
2016-03-08 21:50:07 -0500

    day 17: Initial setup

---
+++ One00Days/One00Days/Day17ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  Day17ViewController.swift
+//  One00Days
+//
+//  Created by Louis Tur on 3/8/16.
+//  Copyright © 2016 SRLabs. All rights reserved.
+//
+
+import UIKit
+
+class Day17ViewController: UIViewController {
+
+  lazy var foldingView: UIView = {
+  let view: UIView = UIView()
+  
+  return view
+  }()
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+  internal func configureConstraints() {
+    
+  }
+  
+  internal func setupViewHierarchy() {
+    
+  }
+
+}
