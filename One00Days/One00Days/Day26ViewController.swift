37fae06adb77e7f3c358970cd4b344d9461781d5
Louis Tur <louis.tur@gmail.com>
2016-03-17 23:32:51 -0400

    day 26: setup... 20 min max, 5 min planning

---
+++ One00Days/One00Days/Day26ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  Day26ViewController.swift
+//  One00Days
+//
+//  Created by Louis Tur on 3/17/16.
+//  Copyright © 2016 SRLabs. All rights reserved.
+//
+
+import UIKit
+
+class Day26ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    
+  }
+  
+  
+  internal func configureConstraints() {
+    
+  }
+  
+  internal func setupViewHierarchy() {
+    
+  }
+  
+}
