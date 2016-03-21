f19633fa07fcbeeb1413977c1a619ce16f66b2e1
Louis Tur <louis.tur@gmail.com>
2016-03-20 21:15:51 -0400

    day 29: setup

---
+++ One00Days/One00Days/Day29ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  Day29ViewController.swift
+//  One00Days
+//
+//  Created by Louis Tur on 3/20/16.
+//  Copyright © 2016 SRLabs. All rights reserved.
+//
+
+import UIKit
+
+class Day29ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  internal func configureConstraints() {
+    
+  }
+  
+  internal func setupViewHierarchy() {
+    
+  }
+  
+  
+}
