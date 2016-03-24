f2b324b07dffa0dcd9826c71c9d233a509b1fc9a
Louis Tur <louis.tur@gmail.com>
2016-03-23 22:28:56 -0400

    day 32: complete. a lot harder to draw fonts than I expected. will need follow up

---
+++ One00Days/One00Days/GridCharacters.swift
@@ -0,0 +1,19 @@
+//
+//  GridCharacters.swift
+//  One00Days
+//
+//  Created by Louis Tur on 3/23/16.
+//  Copyright © 2016 SRLabs. All rights reserved.
+//
+
+import Foundation
+
+class GridCharacters {
+  
+}
+
+class GridH: GridCharacters {
+  
+  // see http://www.makeuseof.com/tag/everything-need-create-fonts-free/
+  
+}
