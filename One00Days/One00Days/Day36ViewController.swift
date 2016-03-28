94fd9900b59f91fa8923bab16da22110a445f2c4
Louis Tur <louis.tur@gmail.com>
2016-03-27 23:21:33 -0400

    day 36: setup, might just do some challenges for a bit

---
+++ One00Days/One00Days/Day36ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  Day36ViewController.swift
+//  One00Days
+//
+//  Created by Louis Tur on 3/27/16.
+//  Copyright © 2016 SRLabs. All rights reserved.
+//
+
+// https://leetcode.com/problems/add-digits/
+// Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+//
+// For example:
+// Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+//
+// Follow up:
+// Could you do it without any loop/recursion in O(1) runtime?
+
+import UIKit
+
+class Day36ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+}
