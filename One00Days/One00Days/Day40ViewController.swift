5e113280b4104abde923909934a401a2df1200be
Louis Tur <louis.tur@gmail.com>
2016-03-31 23:49:29 -0400

    day 40: place holder, shhh... last one

---
+++ One00Days/One00Days/Day40ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Day40ViewController.swift
+//  One00Days
+//
+//  Created by Louis Tur on 3/31/16.
+//  Copyright © 2016 SRLabs. All rights reserved.
+//
+
+import UIKit
+
+
+// last placeholder, I swear. deadlines suck
+class Day40ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+  internal func configureConstraints() {
+    
+  }
+  
+  internal func setupViewHierarchy() {
+    
+  }
+  
+}
